31058bbdda743f7bbeb0584df0a5a4692fd6917c
Aimer Neige <aimer.neige.soft@gmail.com>
2024-03-12 16:02:29 +0000

    init commit

---
+++ src/main/java/com/aimerneige/hellofabric/HelloFabricDataGenerator.java
@@ -0,0 +1,11 @@
+package com.aimerneige.hellofabric;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HelloFabricDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/com/aimerneige/hellofabric/HelloFabricModClient.java
@@ -0,0 +1,10 @@
+package com.aimerneige.hellofabric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HelloFabricModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
